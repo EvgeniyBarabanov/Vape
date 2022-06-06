55686275f80c1991c865e7f7186474f59c113d21
EvgeniyBarabanov <evgeniy.barabanov.96@mail.ru>
2022-06-06 14:59:42 +0300

    vape in process

--- js/script.js
+++ js/script.js
@@ -1,13 +1,51 @@
-const Vape = function(data){//эти данные ожидаются
+const Charger = function(){
+    let status = false;
+    let batteryLevel = 0;
+
+    this.enable = function(){
+        status = true;
+
+        window.timerId = window.setInterval(charger, 500);
+        console.log('device is charging');
+
+        function charger(){
+            if(batteryLevel < 100) batteryLevel += 1;
+            //console.log(`Battery Level ${batteryLevel} %`);
+        };
+    };
+
+    this.disable = function(){
+        status = false;
+
+        window.clearInterval(window.timerId);
+        console.log(`Уровень заряда батареи ${batteryLevel} %`);
+    };
+
+    this.batteryConsumption = function(){
+        return batteryLevel -= 0.5;
+    }
+
+    this.getStatus = function(){
+        return status;
+    };
+
+    this.getBatteryLevel = function(){
+        return batteryLevel;
+    }
+}
+
+
+const Vape = function(data){
+    Charger.apply(this, arguments);
+
+
     let coil = null;
     let liquid = null;
     let isPowered = false;
     let smokingTime = 0;
 
-    
-
     this.addCoil = function(dataCoil){
-        coil = dataCoil
+        coil = dataCoil;
     }
 
     this.addLiquid = function(dataLiquid){
@@ -23,6 +61,10 @@ const Vape = function(data){//эти данные ожидаются
     }
 
     this.buttonOn = function(){
+        if (this.getBatteryLevel() < 1){
+            console.log("battery low");
+            return;
+        }
         if(!isPowered) console.log("Error: vape off");
         if(liquid == null) console.log("Error: add liquid");
         if(coil == null) console.log("Error: add coil");
@@ -33,9 +75,11 @@ const Vape = function(data){//эти данные ожидаются
                 smokingTime += 1;
                 if(liquid.get() > (data.volumeTank * 0.2)){
                     liquid.volumeConsumption();
+                    eleaf1.batteryConsumption();
                     console.log(liquid.get() + "ml");
                 }else if(liquid.get() > 0){
                     liquid.volumeConsumption();
+                    eleaf1.batteryConsumption();
                     console.log("low liquid!");
                 }else if(liquid1.get() <= 0){
                     console.log("coil burned");
@@ -48,7 +92,7 @@ const Vape = function(data){//эти данные ожидаются
     this.buttonOff = function () {
         function stop() {
             window.clearInterval(window.timerId)
-            console.log(`Smoking time: ${smokingTime}s, `);
+            console.log(`Smoking time: ${smokingTime}s `);
         }
         stop();
     }
@@ -64,10 +108,10 @@ const Vape = function(data){//эти данные ожидаются
     this.get = function(){
         return [data, isPowered, coil, liquid];
     }
-
 }
 
 const Coil = function(dataCoil){
+
     this.get = function(){
         return dataCoil;
     }
@@ -75,16 +119,15 @@ const Coil = function(dataCoil){
 
 const Liquid = function(dataLiquid){
     let volume = dataLiquid.volume;
+
     this.get = function(){
         return volume;
     };
+
     this.volumeConsumption = function(){
         return volume -= 0.5;
     }
-
 }
-
-
 
 const eleaf1 = new Vape(
     {
@@ -109,9 +152,11 @@ const liquid1 = new Liquid(
     }
 )
 
-console.log(eleaf1.get());
-eleaf1.vapeOn();
-eleaf1.addCoil(coil1);
-eleaf1.addLiquid(liquid1);
-console.log(eleaf1.get());
 
+//console.log(eleaf1.get());
+//eleaf1.vapeOn();
+//eleaf1.addCoil(coil1);
+//eleaf1.addLiquid(liquid1);
+//console.log(eleaf1.get());
+
+//console.log(eleaf1);
